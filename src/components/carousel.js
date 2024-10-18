ce22363c74c613627e5e1c16e58f658c6fced266
Nemsara Gamage <113989584+MGNemsara@users.noreply.github.com>
2024-10-18 09:45:31 +0300

    added a new banner and updated the display category images

--- src/components/carousel.js
+++ src/components/carousel.js
@@ -5,12 +5,16 @@ import './styles.css';
 // banners
 import ban1 from '../components/pics/banners/ban1.jpg';
 import ban2 from '../components/pics/banners/ban2.jpg';
-import ban3 from '../components/pics/banners/ban3.jpg'
+import ban3 from '../components/pics/banners/ban3.jpg';
+import ban4 from '../components/pics/banners/ban4.jpg';
 
 const CarouselComp = () => (
     <Carousel autoplay>
         <div className="carousel-slide">
         <img src={ban2} alt="Image 1" />
+        </div>
+        <div className="carousel-slide">
+        <img src={ban4} alt="Image 4" />
         </div>
         <div className="carousel-slide">
         <img src={ban1} alt="Image 2" />
