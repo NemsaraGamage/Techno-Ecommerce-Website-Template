e7d7e9477490433bdaa10c196c555b84e4df253f
Nemsara Gamage <113989584+MGNemsara@users.noreply.github.com>
2024-10-18 08:17:33 +0300

    added display category images

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -6,6 +6,12 @@ import Footer from '../Footer';
 // banners
 import Carousel from '../carousel'
 
+// category images 
+import disCata1 from '../pics/introCata/cat1.jpg'
+import disCata2 from '../pics/introCata/cat2.jpg'
+import disCata3 from '../pics/introCata/cat3.jpg'
+import disCata4 from '../pics/introCata/cat4.jpg'
+
 const HomePage = () => {
 
     return (
@@ -16,6 +22,17 @@ const HomePage = () => {
         
         {/* carousel */}
         <Carousel></Carousel>
+
+        {/* display category */}
+
+        <div className='displayCata'>
+
+            <img src={disCata1}></img>
+            <img src={disCata2}></img>
+            <img src={disCata3}></img>
+            <img src={disCata4}></img>
+
+        </div>
 
         </>
     )
