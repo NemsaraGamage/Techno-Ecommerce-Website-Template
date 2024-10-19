69e01f2f49ef50b06ba0b0221f09af7732d4b0c6
Nemsara Gamage <113989584+MGNemsara@users.noreply.github.com>
2024-10-19 21:32:44 +0300

    updated the banners

--- src/components/pages/homePage.js
+++ src/components/pages/homePage.js
@@ -6,11 +6,13 @@ import Footer from '../Footer';
 // banners
 import Carousel from '../carousel'
 
-// category images 
+// intro category images 
 import disCata1 from '../pics/introCata/cat1.jpg'
 import disCata2 from '../pics/introCata/cat2.jpg'
 import disCata3 from '../pics/introCata/cat3.jpg'
 import disCata4 from '../pics/introCata/cat4.jpg'
+import disCata5 from '../pics/introCata/cat5.jpg'
+import disCata6 from '../pics/introCata/cat6.jpg'
 
 // item carousel
 import ItemCarousel from '../itemCarousel1'
@@ -21,6 +23,9 @@ import ItemCarousel5 from '../itemCarousel5'
 
 // mid banner
 import midban1 from '../pics/banners/midBan1.jpg';
+
+// category carousel 
+import cc1 from '../pics/cateCarosuel/cc1.jpg'
 
 const HomePage = () => {
 
@@ -33,12 +38,14 @@ const HomePage = () => {
         {/* carousel */}
         <Carousel></Carousel>
 
-        {/* display category */}
+        {/* intro category */}
         <div className='displayCata'>
             <img src={disCata1}></img>
             <img src={disCata2}></img>
             <img src={disCata3}></img>
             <img src={disCata4}></img>
+            <img src={disCata5}></img>
+            <img src={disCata6}></img>
         </div>
 
         {/* item carousels */}
